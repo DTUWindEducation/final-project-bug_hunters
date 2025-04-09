4346f224a1ff47adc2d410a9f1556031e41b7e4c
Kali Vanderlaan <s242667@dtu.dk>
2025-04-09 22:24:27 +0200

    updating script to also show wind speed time series

--- examples/main.py
+++ examples/main.py
@@ -1,9 +1,10 @@
+#%%
 from pathlib import Path
 import numpy as np 
 import pandas as pd 
 import xarray as xr 
 import matplotlib as plt 
-import WRA_Package as WRA
+import WRA_Package as wra
 
 FILE_PATH = Path(__file__)      # path to this file
 FILE_DIR = FILE_PATH.parent.parent     # path to main folder 
@@ -18,6 +19,23 @@ DATA_15_17 = DATA_DIR / '2015-2017.nc'
 DATA_18_20 = DATA_DIR / '2018-2020.nc'
 DATA_21_23 = DATA_DIR / '2021-2023.nc'
 
-data = WRA.load_data(DATA_00_02)
+wind_speed_with_spatial_dims, wind_dir, time, lat, lon = wra.load_data(DATA_00_02,100)
 
-print(data)
+wind_speed = wind_speed_with_spatial_dims.mean(dim=["latitude", "longitude"]) # I want to come back and look into this further, this should maybe be a function?
+
+wra.plot_wind_speed_histogram(wind_speed,100)
+
+wra.plot_wind_time_series(wind_speed,time,100)
+
+
+
+wind_speed_with_spatial_dims, wind_dir, time, lat, lon = wra.load_data(DATA_00_02,10)
+
+wind_speed = wind_speed_with_spatial_dims.mean(dim=["latitude", "longitude"]) # I want to come back and look into this further, this should maybe be a function?
+
+wra.plot_wind_speed_histogram(wind_speed,10)
+
+wra.plot_wind_time_series(wind_speed,time,10)
+
+
+
