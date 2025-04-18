51b1d22a098eef6ea40c6eaef1fd67db38e78da6
Kali Vanderlaan <s242667@dtu.dk>
2025-04-18 11:43:23 +0200

    commenting out original script to test new loading method

--- examples/main.py
+++ examples/main.py
@@ -19,23 +19,22 @@ DATA_15_17 = DATA_DIR / '2015-2017.nc'
 DATA_18_20 = DATA_DIR / '2018-2020.nc'
 DATA_21_23 = DATA_DIR / '2021-2023.nc'
 
-wind_speed_with_spatial_dims, wind_dir, time, lat, lon = wra.load_data(DATA_00_02,100)
+data_list = [DATA_97_99,DATA_00_02, DATA_03_05, DATA_06_08, DATA_09_11, DATA_12_14, DATA_15_17, DATA_18_20, DATA_21_23]
 
-wind_speed = wind_speed_with_spatial_dims.mean(dim=["latitude", "longitude"]) # I want to come back and look into this further, this should maybe be a function?
-
-wra.plot_wind_speed_histogram(wind_speed,100)
-
-wra.plot_wind_time_series(wind_speed,time,100)
-
-
-
-wind_speed_with_spatial_dims, wind_dir, time, lat, lon = wra.load_data(DATA_00_02,10)
-
-wind_speed = wind_speed_with_spatial_dims.mean(dim=["latitude", "longitude"]) # I want to come back and look into this further, this should maybe be a function?
-
-wra.plot_wind_speed_histogram(wind_speed,10)
-
-wra.plot_wind_time_series(wind_speed,time,10)
+WindData = wra.load_data(DATA_00_02)
+WindDataCumulative = wra.conc_data(data_list)
+# print(lat)
+# print(lon)
+# time_series_data = wra.time_series(wind_speed_data, 55.5, 7.75)
+# print(time_series_data)
+# #wra.plot_wind_speed_histogram(time_series_data,100)
+# wra.plot_wind_time_series(time_series_data,time,100)
 
 
+# wind_speed_data, time, lat, lon = wra.load_data(DATA_00_02,10)
+# time_series_data = wra.time_series(wind_speed_data, 55.5, 7.75)
+# #wra.plot_wind_speed_histogram(time_series_data,10)
+# wra.plot_wind_time_series(time_series_data,time,10)
 
+
+# %%
