810ae47d29da898ac2322a626e5a576b90a50a3b
Kali Vanderlaan <s242667@dtu.dk>
2025-04-29 10:42:54 +0200

    updating main.py to add wind rose figure. Adding input data files to push to remote. Updating .toml file to contain all necessary dependencies. Editing __init__ to addjust for new data files and change method used in the plot wind rose function.

--- examples/main.py
+++ examples/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd 
 import xarray as xr 
 import matplotlib.pyplot as plt 
+from windrose import WindroseAxes
 from matplotlib.pyplot import get_cmap
 from matplotlib.patches import Patch
 import WRA_Package as wra
@@ -15,12 +16,6 @@ DATA_97_99 = DATA_DIR / '1997-1999.nc'
 DATA_00_02 = DATA_DIR / '2000-2002.nc'
 DATA_03_05 = DATA_DIR / '2003-2005.nc'
 DATA_06_08 = DATA_DIR / '2006-2008.nc'
-DATA_09_11 = DATA_DIR / '2009-2011.nc'
-DATA_12_14 = DATA_DIR / '2012-2014.nc'
-DATA_15_17 = DATA_DIR / '2015-2017.nc'
-DATA_18_20 = DATA_DIR / '2018-2020.nc'
-DATA_21_23 = DATA_DIR / '2021-2023.nc'
-
 
 # define locations for each grid point as outlined by assignment
 locations = [(55.5, 7.75), (55.5, 8.00), (55.75, 7.75), (55.75, 8.00)]
@@ -34,7 +29,7 @@ interp_coords = (interpolation_lat, interpolation_long)
 locations.append(interp_coords)
 
 # create list with names of datafiles 
-data_list = [DATA_97_99,DATA_00_02, DATA_03_05, DATA_06_08, DATA_09_11, DATA_12_14, DATA_15_17, DATA_18_20, DATA_21_23]
+data_list = [DATA_97_99,DATA_00_02, DATA_03_05, DATA_06_08]
 
 # concatenate data to create a dataframe containting data for entire time-span  
 WindData = wra.conc_data(data_list)
@@ -46,7 +41,7 @@ time_series_heights = [10, 100]
 # plotting for each coordinate point 
 for height in time_series_heights: 
     for lat, lon in locations: 
-        wra.compute_and_plot_wind_speed_direction_time_series(WindData,lat,lon,height)
+        wra.compute_and_plot_time_series(WindData,lat,lon,height)
 
 
 # --- Extrapolate wind speed to a custom height ---
@@ -67,20 +62,19 @@ ExtrapolatedWindSpeed = pd.DataFrame({'Time': WindData['valid_time']})
 
 for lat, lon in locations[:-1]: 
     # call function to produce dataframe containing wind speed at each location
-    WindSpdDir = wra.compute_and_plot_wind_speed_direction_time_series(WindData,lat, lon, reference_height,display_figure=False)
+    WindSpdDir = wra.compute_and_plot_time_series(WindData,lat, lon, reference_height,display_figure=False)
 
     # access dataframe and extract wind speed at location
     u_ref = WindSpdDir['speed']
 
-    # Extrapolate speed at new height using function
-    extrapolated_speed = wra.extrapolate_wind_speed(u_ref, reference_height, target_height, alpha)
+    # Extrapolate speed at new height using function and append to df
+    ExtrapolatedWindSpeed[f'({lat},{lon})'] = wra.extrapolate_wind_speed(u_ref, reference_height, target_height, alpha)
 
-    # Add speed at each location to dataframe 
-    ExtrapolatedWindSpeed[f'({lat},{lon})'] = extrapolated_speed
+
 
 
 # use function to calculate wind spd and direction for location inside box at 10 m
-WindSpdDirWeibull = wra.compute_and_plot_wind_speed_direction_time_series(WindData,locations[-1][0], locations[-1][1], reference_height,display_figure=False)
+WindSpdDirWeibull = wra.compute_and_plot_time_series(WindData,locations[-1][0], locations[-1][1], reference_height,display_figure=False)
 
 # set reference speed to the wind speed data calculated for location inside box at 10 m
 u_ref_weibull = WindSpdDirWeibull['speed']
@@ -97,12 +91,11 @@ print(f"Shape (k): {shape:.3f}")
 print(f"Scale (A): {scale:.3f}")
 
 # --- Plot histogram with Weibull PDF overlay ---
-fig, ax = wra.plot_wind_speed_with_weibull(extrapolated_speed, shape, scale, level=f"{target_height}m")
+fig, ax = wra.plot_wind_speed_with_weibull(extrapolated_speed_weibull, shape, scale, level=f"{target_height}m")
 plt.show()
-#%%
-# call bin_wind_dir_data to preform binning of directions
-dir_edges, spd_edges, H = wra.windrose_hist(WindSpdDir['direction'], WindSpdDir['speed'], 12)
-wra.plot_windrose(dir_edges, spd_edges, H)
+
+
+wra.plot_wind_rose(WindSpdDir['direction'], WindSpdDir['speed'], num_bins = 8)
 
 
 # %%
